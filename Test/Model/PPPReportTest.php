eaf708aaa8fcee47b40c85d86ee2f57c454e3b55
Lev Lazinskiy <lev@circleci.com>
2016-06-06 20:57:13 -0700

    Initial Tests

---
+++ Test/Model/PPPReportTest.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once 'tests/units/Base.php';
+
+use Kanboard\Core\Plugin\Loader;
+use Kanboard\Plugin\PPP\Model\PPPReport;
+
+class PPPReportTest extends Base
+{
+    public function setup() {
+
+    }
+
+    public function testCreation()
+    {
+        
+    }
+}
