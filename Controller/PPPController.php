0e4347b72bd7c9f4647d66459c0fe95b8ea532bd
Lev Lazinskiy <lev@levlaz.org>
2016-06-13 23:08:55 -0700

    Show all columns

--- Controller/PPPController.php
+++ Controller/PPPController.php
@@ -21,12 +21,16 @@ class PPPController extends BaseController
      {
          $project = $this->getProject();
          $now = date("F j, Y, g:i a");
-         $progress = $this->pppReport->getTasks(13,53);
+         $progress = $this->pppReport->getTasks(1,4);
+         $plans = $this->pppReport->getTasks(1,3);
+         $problems = $this->pppReport->getTasks(1,5);
 
          $this->response->html($this->helper->layout->
             project('ppp:ppp/index', array(
                  'project' => $project,
                  'progress' => $progress,
+                 'plans' => $plans,
+                 'problems' => $problems,
                  'report_date' => $now,
                  'title' => t('PPP'),
                  'values' => array(
